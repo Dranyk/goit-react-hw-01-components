21bd13c71bfc11f0ecf45c8c72abfb36516cf5d4
Dranyk <dvdranyk@gmail.com>
2023-01-14 20:13:53 +0200

    Update StatisticsList.js

--- src/components/Statistics/StatisticsList.js
+++ src/components/Statistics/StatisticsList.js
@@ -5,7 +5,7 @@ import StatisticsListItem from './StatisticsListItem';
 const StatisticsList = ({ title, stats }) => {
     return (
         <section className={css.statistics}>
-        {title?<h2 className={css.title}>{title}</h2>:null}
+        {title && <h2 className={css.title}>{title}</h2>}
       <ul className={css.statList}>
         <StatisticsListItem stats={stats} />
       </ul>
